b6291bc0d42619badf8ce307b3425e834e7c0189
kurilenok <kurilenok@gmail.com>
2017-02-26 22:28:42 +0300

    Second

--- app/src/main/java/numisoft/org/butterknife/MainActivityFragment.java
+++ app/src/main/java/numisoft/org/butterknife/MainActivityFragment.java
@@ -5,11 +5,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindColor;
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    @BindView(R.id.editText)
+    EditText editText;
+
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @BindColor(R.color.colorAccent)
+    int accent;
+
+    @BindColor(R.color.colorPrimaryDark)
+    int dark;
+
+    @BindString(R.string.label)
+    String label;
 
     public MainActivityFragment() {
     }
@@ -17,6 +41,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+
+        textView.setTextColor(accent);
+        textView.setText(label);
+
+        return view;
+    }
+
+    @OnClick(R.id.button)
+    public void onSubmit() {
+        textView.setText(editText.getText());
+        textView.setTextColor(dark);
     }
 }
