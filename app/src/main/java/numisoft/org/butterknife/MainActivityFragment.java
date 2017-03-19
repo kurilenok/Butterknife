0059e1fe999f8abfed93d10cb44f2285b9a66606
kurilenok <kurilenok@gmail.com>
2017-03-19 12:33:17 +0300

    Tabs layout added

--- app/src/main/java/numisoft/org/butterknife/MainActivityFragment.java
+++ app/src/main/java/numisoft/org/butterknife/MainActivityFragment.java
@@ -1,15 +1,21 @@
 package numisoft.org.butterknife;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindColor;
 import butterknife.BindDrawable;
@@ -29,6 +35,12 @@ public class MainActivityFragment extends Fragment {
     @BindView(R.id.textView)
     TextView textView;
 
+    @BindView(R.id.testDbSingle)
+    TextView tvTestDbSingle;
+
+    @BindView(R.id.testDb)
+    TextView tvTestDb;
+
     @BindColor(R.color.colorAccent)
     int accent;
 
@@ -40,9 +52,6 @@ public class MainActivityFragment extends Fragment {
 
     @BindDrawable(R.drawable.coins)
     Drawable coins;
-
-    @BindView(R.id.imageView)
-    ImageView imageView;
 
     public MainActivityFragment() {
     }
@@ -59,10 +68,79 @@ public class MainActivityFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseDatabase fb = FirebaseDatabase.getInstance();
+        DatabaseReference events =
+                fb.getReference().child("events");
+
+        events.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long countSingle = dataSnapshot.getChildrenCount();
+                tvTestDbSingle.setText(Long.toString(countSingle));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        events.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long count = dataSnapshot.getChildrenCount();
+                tvTestDb.setText(Long.toString(count));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        events.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                textView.setText(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
     @OnClick(R.id.button)
     public void onSubmit() {
         textView.setText(editText.getText());
         textView.setTextColor(dark);
-        imageView.setImageDrawable(coins);
+//        imageView.setImageDrawable(coins);
+    }
+
+    @OnClick(R.id.buttonGo)
+    public void goToTabs() {
+        Intent intent = new Intent(getActivity(), TabActivity.class);
+        startActivity(intent);
     }
 }
